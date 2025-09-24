f559647407d29968786f47fdd05a6913089cc09e
byTinus <58219126+byTinus@users.noreply.github.com>
2025-09-24 19:47:47 +0200

    separate example code from main.dart. Setup layout for dashboard

--- lib/dashboard.dart
+++ lib/dashboard.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class DashboardPage extends StatefulWidget {
@@ -8,6 +9,17 @@ class DashboardPage extends StatefulWidget {
 }
 
 class _DashboardPage extends State<DashboardPage> {
+  /// opens the settings window
+  void openSettings() {
+    // TODO. Doesn't work
+    setState(() => showDialog(context: context, builder: (context) => Text("popup")));
+  }
+
+  /// shows a popup, prompting the user to export their data (Y/N)
+  void showExportWindow() {
+    // TODO
+  }
+
   @override
   Widget build(BuildContext context) {
     // Layout:
@@ -19,7 +31,16 @@ class _DashboardPage extends State<DashboardPage> {
     // Calendar
     // Event buttons
     // 
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: MenuItemButton(child: Row(children: 
+        [
+          TextButton(onPressed: openSettings, child: Text("Settings")),
+          TextButton(onPressed: showExportWindow, child: Text("Export data")),
+          //TextButton(onPressed: openSettings, child: Text("Import data")),
+        ],)),
+      ),
+    );
   }
   
 }
